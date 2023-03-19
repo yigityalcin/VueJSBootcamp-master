f32f7151f82aa1267c459566ae2a6764e780ad10
yigityalcin <yigityalciin@hotmail.com>
2023-03-19 16:49:13 +0300

    17:00

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ Vue.createApp({
     },
   },
 }).mount("#app");
-
+nedeneden
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
